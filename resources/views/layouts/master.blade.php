ef78018b2a5c1a8eec3e413f02421e76e92e0f96
erno <waeerno@gmail.com>
2022-11-28 20:33:57 +0700

    title master page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>@yield('title')| Velzon - Admin & Dashboard Template</title>
+    <title>@yield('title')| Peminjaman Asset UIN SUSKA Riau</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
